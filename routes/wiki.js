93404c8ad8d8c5fe122170c3e9559a19fae9e5a8
RWahrman <rebecca.wahrman@gmail.com>
2020-06-25 13:05:39 -0400

    through deleting- quest of whether it should be a get or delete request based on the URL'

--- routes/wiki.js
+++ routes/wiki.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 const { Page, User } = require("../models");
-const { addPage, wikiPage, main } = require("../views");
+const { addPage, wikiPage, main, editPage } = require("../views");
 
 router.get("/", async (req, res, next) => {
   try {
@@ -41,8 +41,65 @@ router.get("/:slug", async (req, res, next) => {
         slug: req.params.slug,
       },
     });
+
+    if (!page) {
+      res.status(404).send(`<h4 style="color: blue">Not Found!!!</h4>`);
+    }
     const author = await page.getAuthor();
     res.send(wikiPage(page, author));
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.get("/:slug/edit", async (req, res, next) => {
+  try {
+    const page = await Page.findOne({
+      where: {
+        slug: req.params.slug,
+      },
+    });
+
+    if (!page) {
+      res.status(404).send(`<h4 style="color: blue">Not Found!!!</h4>`);
+    }
+    const author = await page.getAuthor();
+    res.send(editPage(page, author));
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.post("/:slug", async (req, res, next) => {
+  try {
+    const page = await Page.findOne({
+      where: {
+        slug: req.params.slug,
+      },
+    });
+
+    if (!page) {
+      res.status(404).send(`<h4 style="color: blue">Not Found!!!</h4>`);
+    }
+    let updatedPage = await page.update(req.body);
+    updatedPage = await page.save();
+    console.log("PAGE.SLUG", page.slug);
+    console.log("UPDATEDPAGE.SLUG", updatedPage.slug);
+    res.redirect(`/wiki/${updatedPage.slug}`);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.get("/:slug/delete", async (req, res, next) => {
+  try {
+    const page = await Page.findOne({
+      where: {
+        slug: req.params.slug,
+      },
+    });
+    await page.destroy();
+    res.redirect(`/wiki`);
   } catch (error) {
     next(error);
   }
